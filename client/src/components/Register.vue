a410f1dd368fdd4bdd3682677af5fe656edd57a0
Tsolmon <tsolmon.batjargal64@gmail.com>
2018-02-22 12:40:43 +0800

    register created

---
+++ client/src/components/Register.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="email" v-model="email" />
+    <br>
+    <input type="password" name="password" placeholder="password" v-model="password" />
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data() {
+    return { 
+      email: "abc", 
+      password: "123" 
+      }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data);
+      // console.log('register button was clicked!', this.email, this.password)
+    }
+  }
+/*   watch: {
+    email (value) {
+      console.log('email has changed', value)
+    }
+  }, */
+/*   mounted () {
+    setTimeout(() => {
+      this.email = 'hello world'
+    }, 1000)
+  } */
+}
+</script>
+
+<style scoped></style>
